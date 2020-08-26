52a679bd0393fcab0f71fcc719091a309b9dc01b
Arch-Unique <ikennaidigo@gmail.com>
2020-08-26 12:59:19 +0100

    Fix some errors

--- app/controllers/api/v1/sign_in_controller.rb
+++ app/controllers/api/v1/sign_in_controller.rb
@@ -1,25 +1,29 @@
 class Api::V1::SignInController < ApplicationController
-
-  #post endpoint for sign in
+  # post endpoint for sign in
   def create
-
-    #init param_email
-    param_email = params[:login][:email]
+    # check if params is null
+    # init param_email
+    begin
+      param_email = params[:login][:email]
+    rescue StandardError
+      render json: { status: 'Error', message: 'Empty Parameters' }
+      return
+    end
     param_password = params[:login][:password]
 
-    #make sure we can sign in using both email and full name
-    #TODO will add phone number later
+    # make sure we can sign in using both email and full name
+    # TODO will add phone number later
     user = User.find_by(email: param_email) || User.find_by(full_name: param_email)
 
-    #check if password is correct
-    if user && user.authenticate(param_password)
-      render json: {status: "Success", message: "Signed In Successfully"}
+    # check if password is correct
+    if user&.authenticate(param_password)
+      render json: { status: 'Success', message: 'Signed In Successfully' }
     else
-      render json: {status: "Error", message: "Invalid Email/Password"}
+      render json: { status: 'Error', message: 'Invalid Email/Password' }
+    end
   end
 
   def show
-    render json: {status: "Error", message: "Nothing for now"}
+    render json: { status: 'Error', message: 'Nothing for now' }
   end
-
 end
