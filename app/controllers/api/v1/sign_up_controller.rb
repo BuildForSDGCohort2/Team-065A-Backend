18df5bb0086b1ec6461edadc214d51020d8fd523
Arch-Unique <ikennaidigo@gmail.com>
2020-08-26 11:31:35 +0100

    Create SignIn,SignUp controllers and Users model,database

---
+++ app/controllers/api/v1/sign_up_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::SignUpController < ApplicationController
+  def create
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'sign_up/create'
+      get 'sign_up/show'
+    end
+  end
+  namespace :api do
+    namespace :v1 do
+      get 'sign_in/create'
+      get 'sign_in/show'
+    end
+  end
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
